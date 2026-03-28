86ae1d978e65ef92df58b2b495e752f394b6cecf
JARDO2003 <mjardel934@gmail.com>
2026-03-28 18:13:28 +0100

    Update and rename app.js to firebase-config.js
    r

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js — configuration partagée
+const firebaseConfig = {
+  apiKey: "AIzaSyCPGgtXoDUycykLaTSee0S0yY0tkeJpqKI",
+  authDomain: "data-com-a94a8.firebaseapp.com",
+  databaseURL: "https://data-com-a94a8-default-rtdb.firebaseio.com",
+  projectId: "data-com-a94a8",
+  storageBucket: "data-com-a94a8.firebasestorage.app",
+  messagingSenderId: "276904640935",
+  appId: "1:276904640935:web:9cd805aeba6c34c767f682",
+  measurementId: "G-FYQCWY5G4S"
+};
+
+const VAPID_KEY = "BLrbso554YWcAd7-QPbzRFMUpJ9obsdJitRiFtzr-VU4GArr8ariq1l9klHxFUjSzASpkoelaTDAzcvI7UIWLZ8";
+
+export { firebaseConfig, VAPID_KEY };
