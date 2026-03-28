3330148547e1a5e25f582676bf0cb4593dad9d7e
JARDO2003 <mjardel934@gmail.com>
2026-03-28 18:12:38 +0100

    Update and rename firebase-messaging-sw.js to firebase-messaging-sw.js
    s

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,18 +1,37 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCPGgtXoDUycykLaTSee0S0yY0tkeJpqKI",
   authDomain: "data-com-a94a8.firebaseapp.com",
+  databaseURL: "https://data-com-a94a8-default-rtdb.firebaseio.com",
   projectId: "data-com-a94a8",
+  storageBucket: "data-com-a94a8.firebasestorage.app",
   messagingSenderId: "276904640935",
-  appId: "1:276904640935:web:9cd805aeba6c34c767f682"
+  appId: "1:276904640935:web:9cd805aeba6c34c767f682",
+  measurementId: "G-FYQCWY5G4S"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body
+// Gestion des notifications en arrière-plan
+messaging.onBackgroundMessage((payload) => {
+  console.log('[SW] Message reçu en arrière-plan:', payload);
+
+  const { title, body, icon, image } = payload.notification || {};
+
+  self.registration.showNotification(title || 'Nouvelle notification', {
+    body: body || '',
+    icon: icon || '/icon.png',
+    image: image || undefined,
+    badge: '/badge.png',
+    data: payload.data || {},
+    requireInteraction: true
   });
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  const url = event.notification.data?.url || '/';
+  event.waitUntil(clients.openWindow(url));
 });
